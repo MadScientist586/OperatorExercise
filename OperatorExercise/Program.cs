8f24d3738acaec863e225164115b190d93f701c3
MadScientist586 <mr.scientist.knight@gmail.com>
2022-09-14 10:31:38 -0500

    Created simple calculator

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Please enter a number: ");
+var a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter a second number: ");
+var b = Convert.ToInt32(Console.ReadLine());
+var addition = a += b;
+var subtraction = a -= b;
+var multiplication = a *= b;
+var division = a /= b;
+var modulus = a %= b;
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine($"The sum is {addition}");
+Console.WriteLine($"The difference is {subtraction}");
+Console.WriteLine($"The product is {multiplication}");
+Console.WriteLine($"The quotient is {division}");
+Console.WriteLine($"The remainder is {modulus}");
